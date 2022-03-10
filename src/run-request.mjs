69879dc891e64b85c37c23b3623c72c2a0583448
Gabriel Fernandez <hi.gabe.fz@gmail.com>
2022-03-09 22:46:50 -0500

    Change the way collection name is read

--- src/run-request.mjs
+++ src/run-request.mjs
@@ -1,37 +1,61 @@
 #!/usr/bin/env zx
-import {HOME, GENERATED_DIR, CURRENT_ENV_FILE, COLLECTIONS_DIR, renderTemplate, recursiveResolveTemplating,
-     readFileToString, readFileToObj, writeObjToFile, groupBy} from './modules/common.mjs';
+import {
+    ROOT, OUTPUT_DIR, CURRENT_ENV_FILE, COLLECTIONS_DIR, renderTemplate, recursiveResolveTemplating,
+    readFileToString, readFileToObj, writeObjToFile, groupBy
+} from './modules/common.mjs';
 const path = require("path");
 
 $.verbose = false;
 
-//  input
-const requestFilePath = path.resolve(process.argv[3]);
+await runRequest();
 
-// extract collection id from file path
+async function runRequest() {
 
-const collections = ((await $`ls ${COLLECTIONS_DIR}`).stdout.trim()).split("\n");
-const collection = requestFilePath.replace(COLLECTIONS_DIR, '').split('/')[1];
-if (!collection || !collections.includes(collection))
-    throw 'Could not determine collection from file'
+    //  input
+    const requestFilePath = path.resolve(process.argv[3]);
 
-// read current env for collection
-let currentEnvs = await readFileToObj(CURRENT_ENV_FILE, {});
-let env = {};
-if (currentEnvs[collection])
-    env = await readFileToObj(currentEnvs[collection].generatedEnvFilePath, {});
+    // extract collection id from file path
+    const collection = await getCollectionNameFromFile(requestFilePath);
+    if (!collection)
+        return;
+    
+    // read current env for collection
+    let currentEnvs = await readFileToObj(CURRENT_ENV_FILE, {});
+    let env = {};
+    if (currentEnvs[collection])
+        env = await readFileToObj(currentEnvs[collection].generatedEnvFilePath, {});
 
-// read & render request using environment
-const requestFileObj = renderTemplate(requestFilePath, env);
-if (requestFileObj.body)
-    requestFileObj.body = JSON.stringify(requestFileObj.body);
+    // read & render request using environment
+    const requestFileObj = renderTemplate(requestFilePath, env);
+    if (requestFileObj.body)
+        requestFileObj.body = JSON.stringify(requestFileObj.body);
 
-// send request
+    // send request
 
-const response = await fetch(requestFileObj.url, requestFileObj);
-const data = await response.json();
-console.log(response.ok);
-console.log(response.status);
-console.log(response.statusText);
-console.log(response.headers.raw());
-console.log(JSON.stringify(data, null, 2));
+    const response = await fetch(requestFileObj.url, requestFileObj);
+    const data = await response.json();
+    console.log(response.ok);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers.raw());
+    console.log(JSON.stringify(data, null, 2));
+
+}
+
+async function getCollectionNameFromFile(requestFilePath) {
+    const collections = ((await $`ls ${COLLECTIONS_DIR}`).stdout.trim()).split("\n");
+    const pathArray = requestFilePath.replace(COLLECTIONS_DIR, '').split('/');
+
+    for(var i = 0; i < pathArray.length; i++){
+        if (pathArray[i] === "collections" && i + 1 <= pathArray.length) {
+            collection = pathArray[i+1];
+            break;
+        }
+    }
+
+    if (!collection || !collections.includes(collection))
+        console.log(chalk.red(`could not determine collection from file path`));
+
+    return collection;
+
+}
